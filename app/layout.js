4a3b4ded701efb01dd11d98330c0336b250a7d66
lucaslera91 <lucaslera5@gmail.com>
2023-12-26 02:38:34 -0300

    fix loading and layout

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ProductListAdminProvider } from "@/Contexts/ProductListAdminProvider";
 import AuthProvider from "@/Contexts/AuthProvider";
+import Loading from "./Loading";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +17,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>
-          <ProductListAdminProvider>{children}</ProductListAdminProvider>
+          <ProductListAdminProvider>
+            <Suspense fallback={<Loading />}>{children}</Suspense>
+          </ProductListAdminProvider>
         </AuthProvider>
       </body>
     </html>
